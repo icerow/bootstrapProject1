0f4adae84ce3da823a5d9d658aceae78e6c7182e
Manny Gonzalez <nariamm.gonzalez@gmail.com>
2023-09-23 10:41:01 -0700

    Added intro
    -Added intro
    -made a dark toogle
    -imported pictures
    -changed color scheme
    -added footer and icons w/ links

---
+++ bs5dark.js
@@ -0,0 +1,8 @@
+document.getElementById('toggler').addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'light') {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','light')
+    }
+})
